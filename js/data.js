16aa57e5ce7b7e3fe456281c21107ec66344c622
Thairy <thairydaza@gmail.com>
2021-12-16 13:18:12 -0300

    Agrego acceso a json

---
+++ js/data.js
@@ -0,0 +1,9 @@
+
+const data = require('../data/data.json')
+console.log(data)
+
+/* fetch("../data/data.json")
+.then(response => {
+   return response.json();
+})
+.then(data => console.log(data)); */
