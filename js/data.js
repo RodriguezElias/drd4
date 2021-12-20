1694457a863d7a02026a514790107c29ee611c7a
Thairy <thairydaza@gmail.com>
2021-12-20 16:28:47 -0300

    cambios en cards de tours

--- js/data.js
+++ js/data.js
@@ -18,30 +18,41 @@ const pintarDatos = () => {
     const itemImagen = document.createElement("img")
     itemImagen.setAttribute('src', item.imagen)
     itemImagen.classList.add("item-imagen")
-    const itemTitulo = document.createElement("h3")
+    const itemTitulo = document.createElement("h4")
+    itemTitulo.classList.add("titulo-encima")
     itemTitulo.textContent = `${item.titulo}`
+    const itemProvincia = document.createElement("p")
+    itemProvincia.classList.add("region")
+    itemProvincia.textContent = `${item.provincia}`
+    const itemRegion = document.createElement("p")
+    itemRegion.classList.add("region")
+    itemRegion.textContent = `${item.region}`
+    const itemDescripcion = document.createElement("p")
+    itemDescripcion.classList.add("descripcion")
+    itemDescripcion.textContent = `${item.descripcion}`
+    const itemPrecio = document.createElement("p")
+    itemPrecio.classList.add("precio")
+    itemPrecio.textContent = `${item.precio}`;
     items.appendChild(itemImagen)
     items.appendChild(itemTitulo)
+    items.appendChild(itemProvincia)
+    items.appendChild(itemRegion)
+    items.appendChild(itemDescripcion)
+    items.appendChild(itemPrecio)
     contenedorItems.appendChild(items)
+
   })
 }
-// function recorrerListadoYRenderizarTarjetas() {
-//   //data: data;
-//   let tours = document.querySelector("#obtener-tour");
-//   tours.addEventListener("click", () => {
-//     console.log("hola");
 
-//     let contenedor = document.querySelector("#fila");
-//     productos.forEach((tour) => {
-//       let templateTarjeta = `<div class="caja">
-//       <img src=${tour.imagen} alt=${tour.titulo}>
-//       <p class="info">${tour.titulo}</p>
-//       <p class="info">Provincia: ${tour.provincia}</p>
-//       <p class="info">Región: ${tour.region}</p>
-//       <p class="info">Descripción: ${tour.descripcion}</p>
-//       <p class="info">Precio: ${tour.precio}</p>
-//     </div>`;
-//       contenedor.innerHTML += templateTarjeta;
-//     });
-//   });
-// }
+$('#cuyo').on('click', function() {
+  if( $(this).is(':cuyo') ){
+      //  checkbox ha sido seleccionado
+      propsSelected.push( $(this).val())
+ 
+  } else {
+      //  checkbox ha sido deseleccionado
+      
+      propsSelected = propsSelected.filter(item =>item !== $(this).val() ) 
+  }
+  console.log(propsSelected)
+});
