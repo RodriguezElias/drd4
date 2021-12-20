4a064c726aeaf90beef1ba996521a58c11e581b0
lerodriguez <lerodriguez@gylgroup.com>
2021-12-20 19:16:15 -0300

    se agrego los filtros por region

--- js/data.js
+++ js/data.js
@@ -5,54 +5,77 @@ const traerDatos = async () => {
     const res = await fetch("js/data.json");
     const data = await res.json();
     items = data;
-    pintarDatos();
+    pintarDatos(items);
   } catch (error) {
     console.log(error);
   }
 };
-const pintarDatos = () => {
+
+/* Funcion renderizar datos en el HTML */
+const pintarDatos = (datos) => {
   let contenedorItems = document.querySelector("#datos");
-  items.forEach((item) => {
-    const items = document.createElement("div")
-    items.classList.add("item")
-    const itemImagen = document.createElement("img")
-    itemImagen.setAttribute('src', item.imagen)
-    itemImagen.classList.add("item-imagen")
-    const itemTitulo = document.createElement("h4")
-    itemTitulo.classList.add("titulo-encima")
-    itemTitulo.textContent = `${item.titulo}`
-    const itemProvincia = document.createElement("p")
-    itemProvincia.classList.add("region")
-    itemProvincia.textContent = `${item.provincia}`
-    const itemRegion = document.createElement("p")
-    itemRegion.classList.add("region")
-    itemRegion.textContent = `${item.region}`
-    const itemDescripcion = document.createElement("p")
-    itemDescripcion.classList.add("descripcion")
-    itemDescripcion.textContent = `${item.descripcion}`
-    const itemPrecio = document.createElement("p")
-    itemPrecio.classList.add("precio")
+  datos.forEach((item) => {
+    const items = document.createElement("div");
+    items.classList.add("item");
+    const itemImagen = document.createElement("img");
+    itemImagen.setAttribute("src", item.imagen);
+    itemImagen.classList.add("item-imagen");
+    const itemTitulo = document.createElement("h4");
+    itemTitulo.classList.add("titulo-encima");
+    itemTitulo.textContent = `${item.titulo}`;
+    const itemProvincia = document.createElement("p");
+    itemProvincia.classList.add("region");
+    itemProvincia.textContent = `${item.provincia}`;
+    const itemRegion = document.createElement("p");
+    itemRegion.classList.add("region");
+    itemRegion.textContent = `${item.region}`;
+    const itemDescripcion = document.createElement("p");
+    itemDescripcion.classList.add("descripcion");
+    itemDescripcion.textContent = `${item.descripcion}`;
+    const itemPrecio = document.createElement("p");
+    itemPrecio.classList.add("precio");
     itemPrecio.textContent = `${item.precio}`;
-    items.appendChild(itemImagen)
-    items.appendChild(itemTitulo)
-    items.appendChild(itemProvincia)
-    items.appendChild(itemRegion)
-    items.appendChild(itemDescripcion)
-    items.appendChild(itemPrecio)
-    contenedorItems.appendChild(items)
-
-  })
-}
-
-$('#cuyo').on('click', function() {
-  if( $(this).is(':cuyo') ){
-      //  checkbox ha sido seleccionado
-      propsSelected.push( $(this).val())
- 
-  } else {
-      //  checkbox ha sido deseleccionado
-      
-      propsSelected = propsSelected.filter(item =>item !== $(this).val() ) 
-  }
-  console.log(propsSelected)
+    items.appendChild(itemImagen);
+    items.appendChild(itemTitulo);
+    items.appendChild(itemProvincia);
+    items.appendChild(itemRegion);
+    items.appendChild(itemDescripcion);
+    items.appendChild(itemPrecio);
+    contenedorItems.appendChild(items);
+  });
+};
+/* Funcion agregar eventos a todos los botones filter */
+let filtros = document.querySelectorAll(".filter");
+filtros.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    filtrarDatos(e.target.value, items);
+  });
 });
+
+/* Funcion filtrar los datos del json */
+const filtrarDatos = (orden, array) => {
+  if (orden == "all") {
+    let listaOrdenada = array;
+    let contenedorItems = document.querySelector("#datos");
+    contenedorItems.innerHTML = "";
+    pintarDatos(listaOrdenada);
+  } else {
+    let listaOrdenada = array.filter((prod) => prod.region === orden);
+    let contenedorItems = document.querySelector("#datos");
+    contenedorItems.innerHTML = "";
+    pintarDatos(listaOrdenada);
+  }
+};
+
+// $('#cuyo').on('click', function() {
+//   if( $(this).is(':cuyo') ){
+//       //  checkbox ha sido seleccionado
+//       propsSelected.push( $(this).val())
+
+//   } else {
+//       //  checkbox ha sido deseleccionado
+
+//       propsSelected = propsSelected.filter(item =>item !== $(this).val() )
+//   }
+//   console.log(propsSelected)
+// });
